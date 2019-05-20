1eec7d5e3333ae5ab242206c0e31675c58efbef4
VioletQi <37473275+VioletQi@users.noreply.github.com>
2019-05-20 12:02:28 +0800

    fix

--- ROS_Project/src/move.cpp
+++ ROS_Project/src/move.cpp
@@ -1,17 +1,14 @@
 #include "main.h"
-#include "move.h" 
-void moveback(){
-            srvName.request.name = (nState==STATE_GOTO)?strGoto:"master"; //if
+#include "ExceptionHandler.h"
+
+void moveto(){
+        srvName.request.name = strGoto;
             if (cliGetWPName.call(srvName))
             {
                 std::string name = srvName.response.name;
                 float x = srvName.response.pose.position.x;
                 float y = srvName.response.pose.position.y;
-
-                if(nState==STATE_GOTO)
-                    ROS_INFO("[STATE_GOTO] Get_wp_name = %s (%.2f,%.2f)", strGoto.c_str(),x,y);//if
-                else
-                    ROS_INFO("[STATE_COMEBACK] Get_wp_name = %s (%.2f,%.2f)", strGoto.c_str(),x,y);
+                ROS_INFO("[STATE_GOTO] Get_wp_name = %s (%.2f,%.2f)", strGoto.c_str(),x,y);
 
                 MoveBaseClient ac("move_base", true);
                 if(!ac.waitForServer(ros::Duration(5.0)))
@@ -29,22 +26,14 @@ void moveback(){
                     if(ac.getState() == actionlib::SimpleClientGoalState::SUCCEEDED)
                     {
                         ROS_INFO("Arrived at %s!",strGoto.c_str());
-
-                        if(nState==STATE_GOTO){
-                            Speak("OK. I am taking it.");//if
-                            nState = STATE_GRAB;//if
-                        }
-                        else{
-                            Speak("Hi,master. This is what you wanted.");
-                            nState = STATE_PASS;
-                        }
+                        Speak("OK. I am taking it.");//if
+                        nState = STATE_GRAB;//if
                         nDelay = 0;
                     }
                     else
                     {
                         ROS_INFO("Failed to get to %s ...",strGoto.c_str() );
-                        if(nState==STATE_GOTO) Speak("Failed to go to the waypoint.");//if
-                        else Speak("Failed to go to the master.");
+                        Speak("Failed to go to the waypoint.");//if
                         nState = STATE_ASK;
                     }
                 }
@@ -53,8 +42,52 @@ void moveback(){
             else
             {
                 ROS_ERROR("Failed to call service GetWaypointByName");
-                if(nState==STATE_GOTO) Speak("There is no this waypoint.");//if
-                else  Speak("There is no waypoint named master.");
+                Speak("There is no this waypoint.");//if
+                nState = STATE_ASK;
+            }
+}
+void moveback(){
+    	srvName.request.name = "master";
+            if (cliGetWPName.call(srvName))
+            {
+                std::string name = srvName.response.name;
+                float x = srvName.response.pose.position.x;
+                float y = srvName.response.pose.position.y;
+                ROS_INFO("[STATE_COMEBACK] Get_wp_name = %s (%.2f,%.2f)", strGoto.c_str(),x,y);
+
+                MoveBaseClient ac("move_base", true);
+                if(!ac.waitForServer(ros::Duration(5.0)))
+                {
+                    ROS_INFO("The move_base action server is no running. action abort...");
+                }
+                else
+                {
+                    move_base_msgs::MoveBaseGoal goal;
+                    goal.target_pose.header.frame_id = "map";
+                    goal.target_pose.header.stamp = ros::Time::now();
+                    goal.target_pose.pose = srvName.response.pose;
+                    ac.sendGoal(goal);
+                    ac.waitForResult();
+                    if(ac.getState() == actionlib::SimpleClientGoalState::SUCCEEDED)
+                    {
+                        ROS_INFO("Arrived at %s!",strGoto.c_str());
+                        Speak("Hi,master. This is what you wanted.");
+                        nState = STATE_PASS;
+                        nDelay = 0;
+                    }
+                    else
+                    {
+                        ROS_INFO("Failed to get to %s ...",strGoto.c_str() );
+                        Speak("Failed to go to the master.");
+                        nState = STATE_ASK;
+                    }
+                }
+
+            }
+            else
+            {
+                ROS_ERROR("Failed to call service GetWaypointByName");
+                Speak("There is no waypoint named master.");
                 nState = STATE_ASK;
             }
 }
