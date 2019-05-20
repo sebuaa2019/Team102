3ec623c7fcf7a5eb4a9b01c4d7a4d8807cd85104
16061054 <38604593+16061054@users.noreply.github.com>
2019-05-20 11:44:30 +0800

    Delete unnecessary judge

--- ROS_Project/src/start.cpp
+++ ROS_Project/src/start.cpp
@@ -1,47 +1,42 @@
 #include "main.h" 
 #include "start.h" 
 
-// ����ģʽ����
+// 跟随模式开关
 static void FollowSwitch(bool inActive, float inDist){
 	
-    if(inActive == true){//��������˽������״̬ 
+    if(inActive == true){//命令机器人进入跟随状态 
         srvFlw.request.thredhold = inDist;
-        if (!follow_start.call(srvFlw)){//������������Ϊ�٣���������ʧ�� 
+        if (!follow_start.call(srvFlw)){//跟随启动返回为假，跟随启动失败 
             ROS_WARN("[CActionManager] - follow start failed...");
         }
     }
     
-    else{//���������ֹͣ���� 
-        if (!follow_stop.call(srvFlw)){//ֹͣ���淵��Ϊ�٣�ֹͣ����ʧ�� 
+    else{//命令机器人停止跟随 
+        if (!follow_stop.call(srvFlw)){//停止跟随返回为假，停止跟随失败 
             ROS_WARN("[CActionManager] - failed to stop following...");
         }
     }
 }
 
-void start(){//����׼���׶�
-
-	if(nState == STATE_READY){
-		nDelay ++;//ά��ʱ������������������õ�׼��ʱ��
-		if(nDelay > 100){//׼��ʱ���ѵ���Ĭ��׼���Ѿ����
-			nDelay = 0;//׼����ʱ����������������
-			AddNewWaypoint("start");
-			nState = STATE_FOLLOW;//׼����ɣ��������׶�
-		}
+void start(){//启动准备阶段
+	//启动后延迟一段时间，让机器人做准备
+	nDelay ++;//维护时间计数器，代表了已用的准备时间
+	if(nDelay > 100){//准备时间已到，默认准备已经完成
+		nDelay = 0;//准备计时结束，计数器清零
+		AddNewWaypoint("start");
+		nState = STATE_FOLLOW;//准备完成，进入跟随阶段
 	}
 
 }
 
-void follow(){//����׶�
-
-	if(nState == STATE_FOLLOW){
-		//���������ʱ��Ϊ0����˵������׼���ո����
-		if(nDelay == 0){
-			FollowSwitch(true, 0.7);//��������˽������״̬
-		}
-		//���������ʱ�䲻Ϊ0��˵������������ɣ��������Ѿ����ڸ���״̬
-
-		nDelay ++;//ά��ʱ��������������˴��ڸ���״̬��ʱ��
+void follow(){//跟随阶段
+	//如果计数器时间为0，就说明启动准备刚刚完成
+	if(nDelay == 0){
+		FollowSwitch(true, 0.7);//命令机器人进入跟随状态
 	}
+	//如果计数器时间不为0，说明启动早已完成，机器人已经处于跟随状态
+
+	nDelay ++;//维护时间计数器，代表了处于跟随状态的时间
 
 }
 
