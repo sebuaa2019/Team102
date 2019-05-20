07a239ca42e68c333f3f4f83ba98ca3b6be81e3e
SXX <1136965276@qq.com>
2019-05-21 00:30:41 +0800

    Enhance control over network and remove some sensitive data

--- ROS_Project/netdo/src/netmove.cpp
+++ ROS_Project/netdo/src/netmove.cpp
@@ -1,55 +1,117 @@
 #include <ros/ros.h>
 #include <geometry_msgs/Twist.h>
-#include <sys/socket.h>
-#include <arpa/inet.h>
-#include <unistd.h>
-#include <cstring>
-#include <cstdio>
-#include <cstdlib>
-#include <netdb.h>
+#include <SXXCoreTask/CoreTask.h>
+#include <netprotocol.hpp>
 
+ros::Publisher vel_pub;
 
-int main(int argc, char **argv) {
-    ros::init(argc, argv, "netmove");
-    ros::NodeHandle n;
-    ros::Publisher vel_pub = n.advertise<geometry_msgs::Twist>("/cmd_vel", 10);
-    
-    int port = 13245;
-    struct sockaddr_in cliAddr;
-    struct sockaddr_in servAddr;
-    cliAddr.sin_family = AF_INET;
-    cliAddr.sin_addr.s_addr = htons(0);
+static void retrieveData(void *info) {
+    void *data;
+    int length;
+    ((NetProtocol*)info)->retrieveData_noblock(&data, length);
+    printf("recv%s\n", data);
 
-    int sock = socket(AF_INET, SOCK_STREAM, 0);
+    for (int i = 0; i < length; i++) {
+        char a = ((char*)data)[i];
 
-    bind(sock, (struct sockaddr*)&cliAddr, sizeof(struct sockaddr_in));
+        switch(a) {
+            case 'h':
+            {
+                geometry_msgs::Twist vel_cmd;
 
-    servAddr.sin_family = AF_INET;
-    struct in_addr sin_addr;
-    inet_aton("10.136.117.172", &sin_addr);
-    memcpy(&servAddr.sin_addr, &sin_addr, sizeof(struct in_addr));
+                vel_cmd.linear.x = 0.1;
+                vel_cmd.linear.y = 0;
+                vel_cmd.linear.z = 0;
 
-    servAddr.sin_port = htons(port);
-    socklen_t serLen = sizeof(servAddr);
+                vel_cmd.angular.x = 0;
+                vel_cmd.angular.y = 0;
+                vel_cmd.angular.z = 0;
+                vel_pub.publish(vel_cmd);
+                break;
+            }
+            case 'q':
+            {
+                geometry_msgs::Twist vel_cmd;
 
-    connect(sock, (struct sockaddr*)&servAddr, serLen);
+                vel_cmd.linear.x = 0;
+                vel_cmd.linear.y = 0.1;
+                vel_cmd.linear.z = 0;
 
-    while (ros::ok()) {
-        char a;
-        recv(sock, &a, 1, 0);
-        printf("received: %c\n", a);
+                vel_cmd.angular.x = 0;
+                vel_cmd.angular.y = 0;
+                vel_cmd.angular.z = 0;
+                vel_pub.publish(vel_cmd);
+                break;
+            }
+            case 'w':
+            {
+                geometry_msgs::Twist vel_cmd;
 
-        geometry_msgs::Twist vel_cmd;
+                    vel_cmd.linear.x = -0.1;
+                    vel_cmd.linear.y = 0;
+                    vel_cmd.linear.z = 0;
 
-        vel_cmd.linear.x = 0.1;
-        vel_cmd.linear.y = 0;
-        vel_cmd.linear.z = 0;
+                    vel_cmd.angular.x = 0;
+                    vel_cmd.angular.y = 0;
+                    vel_cmd.angular.z = 0;
+                    vel_pub.publish(vel_cmd);
+                break;
+            }
+            case 'z':
+            {
+                geometry_msgs::Twist vel_cmd;
 
-        vel_cmd.angular.x = 0;
-        vel_cmd.angular.y = 0;
-        vel_cmd.angular.z = 0;
-        vel_pub.publish(vel_cmd);
+                vel_cmd.linear.x = 0;
+                vel_cmd.linear.y = -0.1;
+                vel_cmd.linear.z = 0;
+
+                vel_cmd.angular.x = 0;
+                vel_cmd.angular.y = 0;
+                vel_cmd.angular.z = 0;
+                vel_pub.publish(vel_cmd);
+                break;
+            }
+            case 's':
+            {
+                geometry_msgs::Twist vel_cmd;
+
+                vel_cmd.linear.x = 0;
+                vel_cmd.linear.y = -0.1;
+                vel_cmd.linear.z = 0;
+
+                vel_cmd.angular.x = 0;
+                vel_cmd.angular.y = 0;
+                vel_cmd.angular.z = 0;
+                vel_pub.publish(vel_cmd);
+                break;
+            }
+        }
+        
         ros::spinOnce();
     }
+
+    free(data);
+}
+
+int main(int argc, char **argv) {
+
+    SCT_queue_t queue = SCT_get_global_queue(SCT_QUEUE_PRIORITY_DEFAULT, 0);
+
+    ros::init(argc, argv, "netmove");
+    ros::NodeHandle n;
+    vel_pub = n.advertise<geometry_msgs::Twist>("/cmd_vel", 10);
+
+NetProtocol p;
+
+    p.connectServer();
+    printf("%d\n", p.sock);
+    //fcntl(p.sock, F_SETFL, O_NONBLOCK);
+
+    SCT_source_t server_source = SCT_source_create(SCT_SOURCE_TYPE_READ, p.sock, 0, queue);
+    SCT_set_context(server_source, (void*)&p);
+    SCT_source_set_event_handler_f(server_source, retrieveData);
+    SCT_resume(server_source);
+
+    SCT_main();
     return 0;
 }
